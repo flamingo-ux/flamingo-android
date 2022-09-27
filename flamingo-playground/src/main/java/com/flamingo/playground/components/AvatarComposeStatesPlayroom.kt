0b6328307e57116aeacdbbb94e30afd860c4d87e
whoopzee09 <aleksey.bublyaev@yandex.ru>
2022-09-27 12:07:28 +0300

    @story Added Avatar demo [FLAMINGO-98]

--- flamingo-playground/src/main/java/com/flamingo/playground/components/AvatarComposeStatesPlayroom.kt
+++ flamingo-playground/src/main/java/com/flamingo/playground/components/AvatarComposeStatesPlayroom.kt
@@ -2,7 +2,6 @@ package com.flamingo.playground.components
 
 import android.os.Bundle
 import android.view.View
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -26,14 +25,21 @@ class AvatarComposeStatesPlayroom : PreferenceFragmentCompat() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var content: AvatarContent by mutableStateOf(AvatarContent.Letters('A', 'A', AvatarBackground.PRIMARY))
+        var content: AvatarContent by mutableStateOf(
+            AvatarContent.Letters(
+                'A',
+                'A',
+                AvatarBackground.PRIMARY
+            )
+        )
         var hasOnClick by mutableStateOf(false)
         var size by mutableStateOf(AvatarSize.SIZE_40)
         var shape by mutableStateOf(AvatarShape.CIRCLE)
         var indicator: AvatarIndicator? by mutableStateOf(null)
         var disabled by mutableStateOf(false)
+        var avatarBackground by mutableStateOf(AvatarBackground.PRIMARY)
 
-        // todo figure out a way to initiate Painter on the spot, not inside setComposeDesignComponent
+        // figure out a way to initiate Painter on the spot, not inside setComposeDesignComponent
         var painter: Painter? = null
 
         findPreference<DemoPreference>("component")?.setComposeDesignComponent {
@@ -78,13 +84,14 @@ class AvatarComposeStatesPlayroom : PreferenceFragmentCompat() {
                 (newValue as? String)?.parceNull()?.let {
                     newContent = when (it) {
                         "Image" -> AvatarContent.Image(painter ?: return@onChange false)
-                        "Icon" -> AvatarContent.Icon(Flamingo.icons.Bell, AvatarBackground.PRIMARY)
-                        "Letters" -> AvatarContent.Letters('A', 'A', AvatarBackground.PRIMARY)
+                        "Icon" -> AvatarContent.Icon(Flamingo.icons.Bell, avatarBackground)
+                        "Letters" -> AvatarContent.Letters('A', 'A', avatarBackground)
                         else -> null
                     }
                 }
 
-                //todo change visibilities of corresponding preferences depending on content
+                findPreference("avatarBackground").isVisible =
+                    newContent != null && newContent !is AvatarContent.Image
 
                 if (newContent == null) {
                     return@onChange false
@@ -92,6 +99,91 @@ class AvatarComposeStatesPlayroom : PreferenceFragmentCompat() {
 
                 content = newContent ?: return@onChange false
 
+                true
+            }
+            initPref(
+                savedInstanceState, defVal = AvatarContent.Letters(
+                    'A',
+                    'A',
+                    AvatarBackground.PRIMARY
+                )
+            )
+        }
+
+        configurePreference<DropDownPreference>("avatarBackground") {
+            val list = AvatarBackground.values().map { it.name }.toTypedArray()
+            entries = list
+            entryValues = list
+            onChange { newValue ->
+                val oldContent = content
+                (newValue as? String)?.parceNull()?.let {
+                    avatarBackground = AvatarBackground.valueOf(it)
+                    content = when (oldContent) {
+                        is AvatarContent.Letters -> {
+                            oldContent.copy(background = avatarBackground)
+                        }
+                        is AvatarContent.Icon -> {
+                            oldContent.copy(background = avatarBackground)
+                        }
+                        else -> return@onChange false
+                    }
+                }
+
+                true
+            }
+            initPref(savedInstanceState, defVal = AvatarBackground.PRIMARY)
+        }
+
+        configurePreference<DropDownPreference>("size") {
+            val list = AvatarSize.values().map { it.name }.toTypedArray()
+            entries = list
+            entryValues = list
+            onChange { newValue ->
+                size = AvatarSize.valueOf(newValue)
+                true
+            }
+            initPref(savedInstanceState, defVal = AvatarSize.SIZE_40)
+        }
+
+        configurePreference<DropDownPreference>("shape") {
+            val list = AvatarShape.values().map { it.name }.toTypedArray()
+            entries = list
+            entryValues = list
+            onChange { newValue ->
+                shape = AvatarShape.valueOf(newValue)
+                true
+            }
+            initPref(savedInstanceState, defVal = AvatarShape.CIRCLE)
+        }
+
+        configurePreference<SwitchPreferenceCompat>("indicator") {
+            onChange { newValue ->
+                indicator = if (newValue) AvatarIndicator(IndicatorColor.PRIMARY) else null
+                findPreference("indicatorColor").isVisible = indicator != null
+                findPreference("indicatorIcon").isVisible = indicator != null
+                true
+            }
+            initPref(savedInstanceState, defVal = false)
+        }
+
+        configurePreference<DropDownPreference>("indicatorColor") {
+            val list = IndicatorColor.values().map { it.name }.toTypedArray()
+            entries = list
+            entryValues = list
+            onChange { newValue ->
+                val color = IndicatorColor.valueOf(newValue)
+                val oldIndicator = indicator
+                indicator = oldIndicator?.copy(color = color)
+                true
+            }
+            initPref(savedInstanceState, defVal = IndicatorColor.PRIMARY)
+        }
+
+        configurePreference<SwitchPreferenceCompat>("indicatorIcon") {
+            onChange { newValue ->
+                indicator =
+                    if (newValue) indicator?.copy(icon = Flamingo.icons.Airplay)
+                    else indicator?.copy(icon = null)
                 true
             }
             initPref(savedInstanceState, defVal = false)
