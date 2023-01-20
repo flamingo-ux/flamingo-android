91abae178c46b768c676a568830e65a0ac9d0aeb
whoopzee09 <aleksey.bublyaev@yandex.ru>
2023-01-20 16:52:21 +0300

    @task added ChipGroup Demos [FLAMINGO-58]

--- flamingo/src/main/java/com/flamingo/components/chipgroup/ChipGroupContentLayout.kt
+++ flamingo/src/main/java/com/flamingo/components/chipgroup/ChipGroupContentLayout.kt
@@ -1,15 +1,71 @@
 package com.flamingo.components.chipgroup
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastMap
 
+/**
+ * TODO
+ * __NOTE__ Components inside [content] should have the same height for layout to work properly
+ */
 @Composable
 internal fun ChipGroupContentLayout(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) = Layout(content, modifier) { measurables, constraints ->
-    layout(width = 0, height = 0) { //todo
 
+    require(measurables.isNotEmpty()) {
+        "ChipGroup must have at least 1 Chip!"
+    }
+
+    val placeables = measurables.fastMap {
+        it.measure(Constraints(maxWidth = constraints.maxWidth, maxHeight = constraints.maxHeight))
+    }
+
+    val horizontalPaddingPx = 8.dp.roundToPx()
+    val verticalPaddingPx = 12.dp.roundToPx()
+
+    // todo количество строк не всегда правильно считается, проверить! (если перемешивать чипы в плейграунде, можно наткнуться на этот баг)
+    var totalRows = 1
+    var tmpWidth = 0
+    placeables.forEachIndexed { index, placeable ->
+        tmpWidth += placeable.width
+        if (tmpWidth > constraints.maxWidth) {
+            totalRows++
+            tmpWidth = placeable.width
+        } else if (tmpWidth == constraints.maxWidth && index != placeables.size - 1) {
+            totalRows++
+            tmpWidth = 0
+        } else {
+            tmpWidth += horizontalPaddingPx
+        }
+    }
+
+    Log.d("qwerty", "totalRows $totalRows")
+
+    // NOTE Only works if placeables have the same height!
+    val maxHeight = placeables.maxBy { it.height }.height
+
+    layout(
+        width = constraints.maxWidth,
+        height = totalRows * maxHeight + (totalRows - 1) * verticalPaddingPx
+    ) {
+        var currRow = 0
+        var currX = 0
+        placeables.forEach {
+            if (currX != 0 && currX + it.width > constraints.maxWidth) {
+                currX = 0
+                currRow++
+            }
+            it.placeRelative(
+                x = currX,
+                y = currRow * (maxHeight + verticalPaddingPx)
+            )
+            currX += it.width + horizontalPaddingPx
+        }
     }
 }
